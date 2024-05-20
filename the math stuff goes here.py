2bab17730e58d3d5b7f56a3f1a3e10d11393ab7a
Jayden Knibbs <21032@fraser.school.nz>
2024-05-20 14:26:45 +1200

    20.5.24

--- the math stuff goes here.py
+++ the math stuff goes here.py
@@ -1,5 +1,23 @@
 #the math stuff goes here
 weight = [] 
+supplies = []
+cost = []
+#normi stuff here
+while True:
+    ingrediants = input(' what is an ingrediant in the dish: ')
+    supplies.append(ingrediants)
+    price = input('What is the price of the item?')
+    cost.append(price)
+    choice = input('would you like to add another ingrediant (yes or no): ')
+    if choice == 'no':
+        break
+
+ingrediants_cost = zip(supplies, cost)
+for i in ingrediants_cost:
+    print (i)
+
+list[cost]
+
 ingrdiant_amount_requirement = ('How much of each ingrediant do you need?  please answer in the order of the list being displayed. ')
 print (supplies)
 while True:
